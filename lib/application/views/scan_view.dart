11773b0a8e7be17c86c695c939176b81500d191c
Romain <romain.longeron@gmail.com>
2022-07-24 22:09:39 +0200

    Feat routing navbar

---
+++ lib/application/views/scan_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ScanPage extends StatelessWidget {
+  const ScanPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            Color.fromARGB(255, 107, 23, 81),
+            Color.fromARGB(230, 107, 23, 81),
+            Color.fromARGB(250, 8, 7, 8),
+          ])),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: const Color.fromARGB(0, 0, 0, 0),
+          leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.person),
+          ),
+          actions: [
+            IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+          ],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text(
+                "Connexion",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 25),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
