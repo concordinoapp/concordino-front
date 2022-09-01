d234049fadbe804fb8f6229fcc44634e01e937e5
Romain <romain.longeron@gmail.com>
2022-09-01 16:50:11 +0200

    Feat : implement navbar widget in all page

--- lib/screens/views/home_view.dart
+++ lib/screens/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:concordino_front/constants/colors.dart';
 import 'package:concordino_front/screens/widgets/card_cave.dart';
 import 'package:concordino_front/screens/widgets/card_stat.dart';
+import 'package:concordino_front/screens/widgets/navbar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -95,54 +96,7 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.group),
-              label: 'Home',
-              backgroundColor: Color.fromARGB(255, 131, 4, 11),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Business',
-              backgroundColor: Colors.green,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.camera_alt),
-              label: 'School',
-              backgroundColor: Colors.purple,
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.inventory),
-              label: 'Settings',
-              backgroundColor: Color.fromARGB(255, 131, 4, 11),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.receipt_long),
-              label: 'Settings',
-              backgroundColor: Colors.pink,
-            ),
-          ],
-          onTap: (index) {
-            switch (index) {
-              case 0:
-                Navigator.pushNamed(context, "/home");
-                break;
-              case 1:
-                Navigator.pushNamed(context, "/home");
-                break;
-              case 2:
-                Navigator.pushNamed(context, "/scan");
-                break;
-              case 3:
-                Navigator.pushNamed(context, "/home");
-                break;
-              case 4:
-                Navigator.pushNamed(context, "/list");
-                break;
-            }
-          },
-        ),
+        bottomNavigationBar: const ConcordinoNavbar(),
       ),
     );
   }
